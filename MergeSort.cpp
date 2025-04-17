ca952b1b131822ad5feb16827519256135c13c5f
ZizahSwiftie <azizahulnha@gmail.com>
2025-04-17 10:53:46 +0700

    add struktur mergesort

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -28,3 +28,16 @@ void input()
         cin >> arr[i];
     }
 }
+void mergeSort(int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+
+    int mid = (low + high) / 2;
+
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+}
+
